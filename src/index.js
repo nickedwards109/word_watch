91b39b00483de044a61ec8423ab18bd91553df30
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-10-23 09:24:46 -0600

    Get top word and frequency from the API
    This commit gets the top word and its frequency from the API. This data is not yet appended to the DOM.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const $ = require('jquery');
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('Hello World')
-  $('.word-count').text('Hello World');
+  $.ajax({
+    url: 'https://wordwatch-api.herokuapp.com/api/v1/top_word',
+    method: 'GET'
+  })
+  .then((data) => {
+    const wordAndFrequency = data.word;
+    const word = Object.keys(wordAndFrequency)[0];
+    const frequency = wordAndFrequency[word]
+    // Now we have the data for story 1.
+    // Next step is to append it to the DOM.
+  })
 })
